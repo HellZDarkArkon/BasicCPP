27dc82d8e4c42c99feab3e5753649aefe872cf6f
HellZDarkArkon <maximumwerewolf@gmail.com>
2023-09-01 15:34:19 -0400

    initialized and set variables of 5 data types, integer, float, double, char and string.  then print out all 5 in the console.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,24 @@
 
 int main() {
 
-	std::cout << "Hello, World." << std::endl;
+	int a = 0;
+	float b = 0.0f;
+	double c = 0.0;
+	char d = ' ';
+	std::string e = " ";
+
+	{
+		a = 15;
+		b = 1.0f;
+		c = 0.0002;
+		d = 'e';
+		e = "Hello, World.";
+	}
+
+	std::cout << a << std::endl 
+		<< b << std::endl 
+		<< c << std::endl 
+		<< d << std::endl 
+		<< e << std::endl;
 	return 0;
 }
